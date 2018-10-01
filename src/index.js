df8c30f2f31f133e5f71e0e6de9da15186c6357d
Matteo Gabriele <m.gabriele.dev@gmail.com>
2018-10-01 22:39:39 +0200

    style: remove comments

--- src/index.js
+++ src/index.js
@@ -9,11 +9,6 @@ export default function install (Vue, options = {}) {
   init()
 }
 
-// export { default as query } from '@/lib/query'
-// export { default as config } from '@/lib/config'
-// export { default as page } from '@/lib/page'
-// export { default as disableTracking } from '@/disable-tracking'
-
 export const config = lib.config
 export const page = lib.page
 export const query = lib.query
