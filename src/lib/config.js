f48910fe1892c86ebd6e36c21e5ab476299fcba6
Matteo Gabriele <m.gabriele.dev@gmail.com>
2019-11-20 20:35:51 +0100

    fix(config): id is undefined

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,5 +2,5 @@ import query from "./query";
 import { options } from "../install";
 
 export default (...args) => {
-  query("config", options.id, ...args);
+  query("config", options.config.id, ...args);
 };
