6c4e712bbd4ac4defce3b723d28432a4de2f657f
Matteo Gabriele <m.gabriele.dev@gmail.com>
2019-12-07 16:43:12 +0100

    feat(install): export setOptions
    it is possible to set options in a later moment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import { install } from "./install";
+import { setOptions as _setOptions, install } from "./install";
 import _bootstrap from "./bootstrap";
 
 export default install;
 
 export const bootstrap = _bootstrap;
+export const setOptions = _setOptions;
