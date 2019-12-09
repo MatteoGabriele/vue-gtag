c18415f70ba4b16543c87357b958dffc4cce04f9
Matteo Gabriele <m.gabriele.dev@gmail.com>
2019-12-09 11:16:56 +0100

    refactor(util): deepMerge

--- src/util.js
+++ src/util.js
@@ -31,17 +31,25 @@ export function isObject(item) {
 }
 
 export function mergeDeep(target, ...sources) {
-  if (!sources.length) return target;
+  if (!sources.length) {
+    return target;
+  }
+
   const source = sources.shift();
 
-  if (isObject(target) && isObject(source)) {
-    for (const key in source) {
-      if (isObject(source[key])) {
-        if (!target[key]) Object.assign(target, { [key]: {} });
-        mergeDeep(target[key], source[key]);
-      } else {
-        Object.assign(target, { [key]: source[key] });
+  if (!isObject(target) || !isObject(source)) {
+    return;
+  }
+
+  for (const key in source) {
+    if (isObject(source[key])) {
+      if (!target[key]) {
+        Object.assign(target, { [key]: {} });
       }
+
+      mergeDeep(target[key], source[key]);
+    } else {
+      Object.assign(target, { [key]: source[key] });
     }
   }
 
