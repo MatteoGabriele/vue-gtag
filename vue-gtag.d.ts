975f462dd84a59a07db2a37d5c1d47f054639746
César Valadez <cesar@wdinamo.com>
2020-01-28 16:54:30 -0600

    Implement gtag.js types. Adding missing options & methods

--- vue-gtag.d.ts
+++ vue-gtag.d.ts
@@ -1,6 +1,11 @@
-declare module 'vue-gtag' {
-  import _Vue, { PluginFunction } from 'vue';
-  import VueRouter from 'vue-router';
+declare module "vue-gtag" {
+  import _Vue, { PluginFunction } from "vue";
+  import VueRouter from "vue-router";
+
+  type GtagEventParams =
+    | Gtag.ControlParams
+    | Gtag.EventParams
+    | Gtag.CustomParams;
 
   export interface PageView {
     page_title: string;
@@ -8,10 +13,10 @@ declare module 'vue-gtag' {
     page_path: string;
   }
 
-  export interface Event {
+  export interface Event extends Gtag.EventParams {
     event_category: string;
     event_label: string;
-    value: string;
+    value: number;
   }
 
   export interface ScreenView {
@@ -21,12 +26,20 @@ declare module 'vue-gtag' {
 
   export interface Purchase {
     transaction_id: string;
-    affiliation: string;
-    value: number;
+    affiliation?: string;
+    value?: number;
+    tax?: number;
+    shipping?: number;
+    items?: Gtag.Item[];
+    checkout_step?: number;
+    checkout_option?: string;
   }
 
   export interface Linker {
     domains: string[];
+    decorate_forms?: boolean;
+    accept_incoming?: boolean;
+    url_position?: "fragment" | "query";
   }
 
   export interface Exception {
@@ -34,21 +47,39 @@ declare module 'vue-gtag' {
     fatal: boolean;
   }
 
+  export interface Timing {
+    name: string;
+    value: number;
+    event_category?: string;
+    event_label?: string;
+  }
+
   export type Dictionary<T> = { [key: string]: T };
 
   export interface VueGtag {
+    optIn(): void;
+    optOut(): void;
     pageview(pageView: PageView): void;
-    event(action: string, event: Event): void;
+    event(action: Gtag.EventNames | string, event: Event): void;
     screenview(screenView: ScreenView): void;
     customMap(map: Dictionary<string>): void;
+    purchase(puchase: Event): void;
     purchase(purchase: Purchase): void;
     linker(config: Linker): void;
     exception(ex: Exception): void;
+    set(config: Gtag.CustomParams): void;
+    config(config?: GtagEventParams);
+    time(timing: Timing);
+  }
+
+  export interface DomainConfig {
+    id: string;
+    params?: GtagEventParams & { send_page_view: boolean };
   }
 
   export interface PluginOptions {
     appName?: string;
-    pageTrackerTemplate?: () => void;
+    pageTrackerTemplate?: () => PageView;
     onBeforeTrack?: () => void;
     onAfterTrack?: () => void;
     onReady?: () => void;
@@ -59,20 +90,24 @@ declare module 'vue-gtag' {
     pageTrackerEnabled?: boolean;
     pageTrackerScreenviewEnabled?: boolean;
     defaultGroupName?: string;
-    includes?: any;
-    config?: {
-      id: string,
-      params?: Dictionary<any>
-    };
+    includes?: DomainConfig[];
+    config?: DomainConfig;
   }
 
   export class VueGtagPlugin {
-    static install(Vue: typeof _Vue, options: PluginOptions, router?: VueRouter): void;
+    static install(
+      Vue: typeof _Vue,
+      options: PluginOptions,
+      router?: VueRouter
+    ): void;
   }
+
+  export function bootstrap(): Promise<Gtag.Gtag>;
+  export function setOptions(PluginOptions): void;
 
   export default VueGtagPlugin;
 
-  module 'vue/types/vue' {
+  module "vue/types/vue" {
     interface Vue {
       $ga: VueGtag;
     }
