e96bdb7f55735a082ff6365f77a2e29451c524fe
Matteo Gabriele <m.gabriele.dev@gmail.com>
2020-02-17 09:42:00 +0100

    fix(pageview): wrong event parameters passed

--- src/api/pageview.js
+++ src/api/pageview.js
@@ -1,5 +1,17 @@
 import event from "./event";
 
 export default (...args) => {
-  event("page_view", ...args);
+  const [arg] = args;
+  let params = {};
+
+  if (typeof arg === "string") {
+    params = {
+      page_path: arg,
+      page_location: window.location.href
+    };
+  } else {
+    params = arg;
+  }
+
+  event("page_view", params);
 };
