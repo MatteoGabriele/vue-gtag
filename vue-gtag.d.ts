2016e2ef5ddacac33848eaac1adc5ab133923158
Hugo Jeanmaire <hjeanmaire@fairplayer.co>
2023-12-07 11:20:22 +0100

    Revert "Add custom url to vue gtag options"
    This reverts commit 3e2009a8f2ecdd171991bf42b17766be5e6c9c93.

--- vue-gtag.d.ts
+++ vue-gtag.d.ts
@@ -271,7 +271,6 @@ declare module "vue-gtag" {
     onBeforeTrack?: () => void;
     onAfterTrack?: () => void;
     onReady?: () => void;
-    customResourceURL: string,
     enabled?: boolean;
     disableScriptLoad?: boolean;
     bootstrap?: boolean;
