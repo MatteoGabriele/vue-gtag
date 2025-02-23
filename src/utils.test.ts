85604910309410a9d238d84c5faed1a8a081e6bd
MatteoGabriele <m.gabriele.dev@gmail.com>
2025-02-23 17:03:47 +0100

    feat: add injectScript test

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,3 +1,47 @@
+import * as utils from "@/utils";
+import flushPromises from "flush-promises";
+
 describe("utils", () => {
-  describe.todo("injectScript", () => {});
+  describe("injectScript", () => {
+    afterEach(() => {
+      document.getElementsByTagName("html")[0].innerHTML = "";
+    });
+
+    it("should create a script tag", async () => {
+      utils.injectScript("foo");
+
+      await flushPromises();
+
+      const scripts = document.getElementsByTagName("script");
+
+      expect(scripts.length).toEqual(1);
+      expect(scripts[0].getAttribute("src")).toEqual("foo");
+      expect(scripts[0].defer).toEqual(false);
+    });
+
+    it("should create a script tag with the defer attribute", async () => {
+      utils.injectScript("foo", {
+        defer: true,
+      });
+
+      await flushPromises();
+
+      const scripts = document.getElementsByTagName("script");
+
+      expect(scripts[0].defer).toEqual(true);
+    });
+
+    it("should create a link for domain preconnect", async () => {
+      utils.injectScript("foo", {
+        preconnectOrigin: "preconnect_origin_value",
+      });
+
+      await flushPromises();
+
+      const links = document.getElementsByTagName("link");
+
+      expect(links[0].getAttribute("href")).toEqual("preconnect_origin_value");
+      expect(links[0].getAttribute("rel")).toEqual("preconnect");
+    });
+  });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,37 @@
 export async function injectScript(
-  resource: string,
-  options: {
+  url: string,
+  options?: {
     preconnectOrigin?: string;
     defer?: boolean;
   },
 ): Promise<void> {
-  if (typeof document === "undefined") {
-    return;
-  }
+  return new Promise((resolve, reject) => {
+    if (typeof document === "undefined") {
+      return;
+    }
+
+    const head = document.head || document.getElementsByTagName("head")[0];
+    const script = document.createElement("script");
+
+    script.async = true;
+    script.src = url;
+
+    if (options?.defer) {
+      script.defer = true;
+    }
+
+    if (options?.preconnectOrigin) {
+      const link = document.createElement("link");
+
+      link.href = options.preconnectOrigin;
+      link.rel = "preconnect";
+
+      head.appendChild(link);
+    }
+
+    head.appendChild(script);
+
+    script.onload = () => resolve;
+    script.onerror = reject;
+  });
 }
