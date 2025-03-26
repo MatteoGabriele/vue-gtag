012973a4dc52bb9a84679366e02a886e6f800fba
MatteoGabriele <m.gabriele.dev@gmail.com>
2025-03-26 18:33:09 +0100

    test: make sure to check global properties

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,5 @@
+import { createApp } from "vue";
+import * as api from "./api/index";
 import addGtag from "./core/add-gtag";
 import { createGtag } from "./index";
 
@@ -7,5 +9,14 @@ describe("index", () => {
   it("should install the plugin", () => {
     createGtag({ tagId: "UA-123456789" });
     expect(addGtag).toHaveBeenCalled();
+  });
+
+  it("should install global properties", () => {
+    const app = createApp({});
+    const gtag = createGtag({ tagId: "UA-123456789" });
+
+    app.use(gtag);
+
+    expect(app.config.globalProperties.$gtag).toEqual(api);
   });
 });
